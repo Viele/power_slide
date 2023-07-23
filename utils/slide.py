1f9ff01fc9c3e3e6a3488b35a91bd5800122bee8
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-07-23 14:31:52 +0200

    fix bug on fresh scene

--- utils/slide.py
+++ utils/slide.py
@@ -40,7 +40,7 @@ def get_slide_collection(context: bpy.types.Context) -> bpy.types.LayerCollectio
         return view_layer.layer_collection.children[_SLIDE_COLLECTION]
     
     slide_collection = bpy.data.collections.new(_SLIDE_COLLECTION)
-    view_layer.layer_collection.children.link(slide_collection)
+    view_layer.layer_collection.collection.children.link(slide_collection)
 
     return view_layer.layer_collection.children[_SLIDE_COLLECTION]
 
