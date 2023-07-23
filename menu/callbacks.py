ae6540b81544f1632f5730d8fa8fa04e37bf1db6
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-07-23 13:08:33 +0200

    refactor callback gui

--- menu/callbacks.py
+++ menu/callbacks.py
@@ -1,5 +1,4 @@
 import bpy
-from bpy.types import Context
 from ..utils import (
     slide as _slide_utils, 
     callbacks as _callback_utils
@@ -8,23 +7,32 @@ from ..utils import (
 from ..callbacks import main as _cb_main
 
 
+def _draw_callback_gui(context: bpy.types.Context, layout: bpy.types.UILayout, callback_list: str):
+    active_slide = _slide_utils.get_current_slide(context)
+    layout.label(text=f"Callbacks for slide: '{active_slide.name}'")
+    layout.template_list(
+        "PSL_UL_callbacks", "", 
+        getattr(active_slide.collection, callback_list), "callbacks", 
+        getattr(active_slide.collection, callback_list), "active_index")
+    row = layout.row()
+    op = row.operator("psl.create_callback")
+    op.callback_list = callback_list
+    op = row.operator("psl.delete_callback")
+    op.callback_list = callback_list
+
+    callbacks = _callback_utils.get_callbacks(context, callback_list)
+    for cb in callbacks:
+        box = layout.box()
+        box.label(text=_cb_main.get_list_name(cb))
+        _cb_main.draw(cb, context, box)
+
+
 class PSL_PT_Callbacks(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_label = "Callbacks"
+    bl_label = "Callbacks - On Enter"
     bl_category = "Power Slide"
 
-    def draw(self, context: Context):
+    def draw(self, context: bpy.types.Context):
         layout = self.layout
-        active_slide = _slide_utils.get_current_slide(context)
-        layout.label(text=f"Callbacks for slide: '{active_slide.name}'")
-        layout.template_list("PSL_UL_callbacks", "", active_slide.collection.on_enter, "callbacks", active_slide.collection.on_enter, "active_index")
-        row = layout.row()
-        row.operator("psl.create_callback")
-        row.operator("psl.delete_callback")
-
-        callbacks = _callback_utils.get_callbacks(context, "on_enter")
-        for cb in callbacks:
-            box = layout.box()
-            box.label(text=_cb_main.get_list_name(cb))
-            _cb_main.draw(cb, context, box)
+        _draw_callback_gui(context, layout, "on_enter")
